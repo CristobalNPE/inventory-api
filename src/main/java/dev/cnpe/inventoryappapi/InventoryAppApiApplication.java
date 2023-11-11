fe42c6d21e8d5d05b1c2d876ecdd0bd567fe3ee7
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-11-11 15:44:22 -0300

    create base project

---
+++ src/main/java/dev/cnpe/inventoryappapi/InventoryAppApiApplication.java
@@ -0,0 +1,13 @@
+package dev.cnpe.inventoryappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cnpe/inventoryappapi/InventoryAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cnpe.inventoryappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
