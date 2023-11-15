f4ddf18b418760e69a7d96d26b1b6455dbd1360a
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-11-15 02:20:29 -0300

    add methods for info retrieval

--- src/main/java/dev/cnpe/inventoryappapi/repositories/CategoryRepository.java
+++ src/main/java/dev/cnpe/inventoryappapi/repositories/CategoryRepository.java
@@ -2,13 +2,18 @@ package dev.cnpe.inventoryappapi.repositories;
 
 import dev.cnpe.inventoryappapi.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
   Optional<Category> findByName(String name);
 
   boolean existsByName(String name);
+
+  @Query("SELECT c FROM Category c WHERE c.categoryItems IS EMPTY")
+  Set<Category> findCategoriesWithNoItems();
 
 }
--- src/main/java/dev/cnpe/inventoryappapi/repositories/ItemRepository.java
+++ src/main/java/dev/cnpe/inventoryappapi/repositories/ItemRepository.java
@@ -2,6 +2,25 @@ package dev.cnpe.inventoryappapi.repositories;
 
 import dev.cnpe.inventoryappapi.domain.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ItemRepository extends JpaRepository<Item,Long> {
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+
+  @Query("SELECT i FROM Item i WHERE i.stock < 5")
+  Set<Item> findItemsWithLowStock();
+
+  @Query("SELECT SUM(i.stock) FROM Item i")
+  Long sumOfStock();
+
+  @Query("SELECT SUM(i.price * i.stock) FROM Item i")
+  BigDecimal totalValue();
+
+
+  Optional<Item> findTop1ByOrderByStockDesc();
+
 }
